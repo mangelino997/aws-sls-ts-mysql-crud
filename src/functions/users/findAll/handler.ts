7eb0853e765824b7737706ab70af2c55e7c7a218
Marcio Angelino <37160293+mangelino997@users.noreply.github.com>
2024-02-08 13:33:07 -0300

    first commit

---
+++ src/functions/users/findAll/handler.ts
@@ -0,0 +1,18 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import schema from './schema';
+
+const findAll: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event, context: Context) => {
+  const { name } = event.body;
+  const { awsRequestId } = context;
+  console.log(awsRequestId);
+  return formatJSONResponse({
+    message: `Hello ${name}, welcome to the exciting Serverless world!`,
+    event,
+  });
+};
+
+export const main = middyfy(findAll);
